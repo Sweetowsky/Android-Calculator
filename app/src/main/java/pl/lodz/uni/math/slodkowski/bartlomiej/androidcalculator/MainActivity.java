8169599e31497838cc3187274de11fe55d314a6d
Sweetowsky <barkosrm7@gmail.com>
2019-02-16 12:39:31 +0100

    Added library - working calculator

--- app/src/main/java/pl/lodz/uni/math/slodkowski/bartlomiej/androidcalculator/MainActivity.java
+++ app/src/main/java/pl/lodz/uni/math/slodkowski/bartlomiej/androidcalculator/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 public class MainActivity extends AppCompatActivity {
 
     private StringBuilder textEquation = new StringBuilder();
@@ -22,17 +25,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clearAllSign(View view ){
+        int lengthEquation = textEquation.length();
+        if (lengthEquation < 1) {
+            refreshText();
+        } else {
+            textEquation.delete(0, textEquation.length());
+            refreshText();
+        }
 
     }
     public void clearLastSign(View view ){
+        int lengthEquation = textEquation.length();
+        if (lengthEquation < 1) {
+            refreshText();
+        } else {
+            textEquation.delete(textEquation.length() - 1, textEquation.length());
+            refreshText();
+        }
 
     }
 
     public void addToEquation(View view){
-        int lengthOfEguqtion = textEquation.length() - 1;
+        int lengthOfEquation = textEquation.length() - 1;
         TextView button = (TextView) view;
         String character = button.getText().toString();
-        if (lengthOfEguqtion > 13) {
+        if (lengthOfEquation > 13) {
             return;
         }
         textEquation.append(character);
@@ -44,6 +61,21 @@ public class MainActivity extends AppCompatActivity {
     }
     public void makeCalculation(View view)
     {
+        int lengthEquation = textEquation.length();
+        if (lengthEquation < 1) {
+            return;
+        }
+        double result;
+        try {
+            Expression expression = new ExpressionBuilder(textEquation.toString()).build();
+            result = expression.evaluate();
+        } catch (Exception e) {
+            result = Double.NaN;
+        }
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(String.valueOf(result));
+        textEquation.delete(0, textEquation.length());
 
 
     }
